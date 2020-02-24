1c88218bbb341ba0d73f24106972066105f238cd
homer <15629039251@163.com>
2020-02-24 09:29:32 +0800

    0.0.9

--- K12FlutterPod.podspec
+++ K12FlutterPod.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "K12FlutterPod"
-  spec.version      = "0.0.8"
+  spec.version      = "0.0.9"
   spec.summary      = "A summary of K12FlutterPod."
 
   # This description is used to generate tags and improve search results.
--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,10 @@
 # use download to replace moving
+if
 cp -r /Users/homerwang/projects/demos/Frameworks ./Frameworks
+then
+  echo "copy frameworks"
+else
+  echo "start download frameworks"
+  curl -o Frameworks.zip https://note-1254003320.cos.ap-guangzhou.myqcloud.com/frameworks/Frameworks.zip
+  unzip Frameworks.zip
+fi
